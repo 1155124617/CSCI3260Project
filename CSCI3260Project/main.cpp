696b4c2faf1c0c46afdb23ac64989cd450ce214e
1155124617 <1155124617@link.cuhk.edu.hk>
2021-11-22 15:07:11 +0800

    New struture

---
+++ CSCI3260Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSCI3260Project
+//
+//  Created by walter on 22/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
