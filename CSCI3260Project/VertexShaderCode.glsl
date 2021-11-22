948b3ad639c4453da138ac136a35b79e073aaf3c
Zijie Dai <64151544+zijiedai@users.noreply.github.com>
2021-11-22 16:46:39 +0800

    Shaders
    The fragment shader only uses the directional light

---
+++ CSCI3260Project/VertexShaderCode.glsl
@@ -0,0 +1,22 @@
+
+#version 430
+in layout(location=0) vec3 position;
+in layout(location=1) vec2 uv;
+in layout(location=2) vec3 normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+
+out vec2 uvCoord;
+out vec3 normalWorld;
+out vec3 vertexWorld;
+
+void main()
+{	
+	gl_Position=projection*view*model*vec4(position,1.0);
+	normalWorld=mat3(model)*normal;
+	vertexWorld=vec3(model*vec4(position,1.0f));
+	uvCoord=uv;
+}
